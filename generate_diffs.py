c6f3d6050e09de6ee915734f82eef0065c8e8bb6
Mahir-Sparkess <mahirrahman2000@gmail.com>
2021-08-03 09:14:07 +0100

    transformed script to function

--- generate_diffs.py
+++ generate_diffs.py
@@ -5,47 +5,50 @@ from datetime import datetime
 parser = argparse.ArgumentParser(description="Generate a list of dictionaries containing events")
 parser.add_argument('source-index', help="name of source index")
 parser.add_argument('dest-index', help="name of destination index")
-args = parser.parse_args(["opensearch-collections-difftest", "opensearch-collections-difftest-dest"])
-
+args = parser.parse_args()
 es = CEDAElasticsearchClient()
 
 
-res = es.search(index=vars(args)['source-index'], body={"query": {"match_all": {}}})
-source_hits = [hit['_source'] for hit in res['hits']['hits']]
-source_set = set([hit['collection_id'] for hit in source_hits])
+def get_events(source_index: str, dest_index: str) -> list[dict]:
+    res = es.search(index=source_index, body={"query": {"match_all": {}}})
+    source_hits = [hit['_source'] for hit in res['hits']['hits']]
+    source_set = set([hit['collection_id'] for hit in source_hits])
 
-res = es.search(index=vars(args)['dest-index'], body={"query": {"match_all": {}}})
-dest_hits = [hit['_source'] for hit in res['hits']['hits']]
-dest_set = set([hit['collection_id'] for hit in dest_hits])
+    res = es.search(index=dest_index, body={"query": {"match_all": {}}})
+    dest_hits = [hit['_source'] for hit in res['hits']['hits']]
+    dest_set = set([hit['collection_id'] for hit in dest_hits])
 
-added = dest_set - source_set
-removed = source_set - dest_set
+    added = dest_set - source_set
+    removed = source_set - dest_set
 
-events = []
+    events = []
 
-for additions in added:
-    hit = next(hit for hit in dest_hits if hit['collection_id'] == additions)
-    events.append(
-        {
-            'collection_id' : hit['collection_id'],
-            'collection_title' : hit['title'],
-            'action' : 'added',
-            'datetime' : datetime.now().isoformat(),
+    for additions in added:
+        hit = next(hit for hit in dest_hits if hit['collection_id'] == additions)
+        events.append(
+            {
+                'collection_id': hit['collection_id'],
+                'collection_title': hit['title'],
+                'action': 'added',
+                'datetime': datetime.now().isoformat(),
 
-        }
-    )
+            }
+        )
 
-for removals in removed:
-    hit = next(hit for hit in source_hits if hit['collection_id'] == removals)
-    events.append(
-        {
-            'collection_id': hit['collection_id'],
-            'collection_title': hit['title'],
-            'action': 'removed',
-            'datetime': datetime.now().isoformat(),
+    for removals in removed:
+        hit = next(hit for hit in source_hits if hit['collection_id'] == removals)
+        events.append(
+            {
+                'collection_id': hit['collection_id'],
+                'collection_title': hit['title'],
+                'action': 'removed',
+                'datetime': datetime.now().isoformat(),
 
-        }
-    )
+            }
+        )
+    
+    print(events)
+    return events
 
-print(events)
 
+get_events(vars(args)['source-index'], vars(args)['dest-index'])
